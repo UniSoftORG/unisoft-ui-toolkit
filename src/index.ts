766bf12394ba4e2ec40e64e008ed7e52ab36c430
PIYAtGitHub <petyr.yonkov@mail.bg>
2022-11-01 00:01:17 +0200

    try to export a type via index

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,14 @@ import pageWithAuth, {elementWithAuth} from './guards/authGuard';
 import { ApiRequest } from './api/apiRequest';
 import { isGenericApiError } from './guards/genericApiError';
 import { isLoginResponse } from './guards/loginResponse';
+import { SupportedApiMethods } from './interfaces/generics';
 
 const authGuard =  {pageWithAuth, elementWithAuth}; 
 const permissionsGuard =  {pageWithPermissions, elementWithPermissions}; 
 const typeGuard = {isGenericApiError, isLoginResponse}
 
+const commonTypes = {SupportedApiMethods}
+
 const api = new ApiRequest(); // a singllke instance to use 
 
-export { sayHi, authGuard, permissionsGuard, typeGuard, api}; 
+export { sayHi, authGuard, permissionsGuard, typeGuard, api, commonTypes}; 
