e5c87e8d7f6ae10eb9e942ca5cccf4e8339d773e
PIYAtGitHub <petyr.yonkov@mail.bg>
2022-10-31 22:48:38 +0200

    get type guards in as well

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,13 @@ import { sayHi } from './hi'
 import elementWithPermissions, { pageWithPermissions }  from './guards/permissionsGuard'; 
 import pageWithAuth, {elementWithAuth} from './guards/authGuard';
 import { ApiRequest } from './api/apiRequest';
+import { isGenericApiError } from './guards/genericApiError';
+import { isLoginResponse } from './guards/loginResponse';
 
 const authGuard =  {pageWithAuth, elementWithAuth}; 
 const permissionsGuard =  {pageWithPermissions, elementWithPermissions}; 
+const typeGuard = {isGenericApiError, isLoginResponse}
+
 const api = new ApiRequest(); // a singllke instance to use 
 
-export { sayHi, authGuard, permissionsGuard, api}; 
+export { sayHi, authGuard, permissionsGuard, typeGuard, api}; 
