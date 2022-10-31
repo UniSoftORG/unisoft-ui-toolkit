e8fab37afe0c419fecc865122e5f2f5e59f24ccb
PIYAtGitHub <petyr.yonkov@mail.bg>
2022-10-31 09:02:09 +0200

    link through index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
 import { sayHi } from './hi'
+import elementWithPermissions, { pageWithPermissions }  from './guards/permissionsGuard'; 
+import pageWithAuth, {elementWithAuth} from './guards/authGuard';
+import { ApiRequest } from './api/apiRequest';
 
-export { sayHi }
+const authGuard =  {pageWithAuth, elementWithAuth}; 
+const permissionsGuard =  {pageWithPermissions, elementWithPermissions}; 
+const api = new ApiRequest(); // a singllke instance to use 
+
+export { sayHi, authGuard, permissionsGuard, api}; 
