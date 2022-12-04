129ab100ba5d12820580a0745e32c1b919ca1c6d
PIYAtGitHub <petyr.yonkov@mail.bg>
2022-12-04 10:18:10 +0200

    [IMPORTANT] apiRequest now requires full URL, no hardcoded base!

--- src/api/apiRequest.ts
+++ src/api/apiRequest.ts
@@ -1,7 +1,19 @@
 import { SupportedApiMethods } from "../interfaces/generics";
 import { IBreadcrumbs } from "../interfaces/breadCrumbs";
 
-/// Global api request class. Use this to put any api request through
+/**
+ * Global api request class. Use this to put any api request through
+ *
+ * It needs to know of a token, which means you need to create one instance without token (pre auth)
+ * and one instance with a token (post auth). Otherwise you are doomed.
+ *
+ * On the createRequest method: this one will take a FULL url. For example api.staging.gg/admin/createUser.
+ *
+ * Before it used to be a lot simpler intake, limited to the endpoint itself, such as /admin/createUser.
+ *
+ * However, since this is an abstraction, and yu can't or don't need to put the env here as well, you will need
+ * to mod the requests as per this specification. Thank you!
+ *  */
 export class ApiRequest {
   token: string;
   constructor(token: string) {
@@ -11,20 +23,23 @@ export class ApiRequest {
   setToken = (newToken: string) => {
     if (!newToken.length) throw new Error("bad data!");
 
-    console.log(">>>>>>> NEW TOKEN HAS BEEN SET UP >>>>>>>");
+    console.log("[DEBUG TBD] NEW TOKEN HAS BEEN SET UP >>>>>>>");
     this.token = newToken;
   };
 
   requestHeaders = (additionalHeaders?: { [key: string]: string }) => {
+    console.log("[DEBUG TBD] About to set headers", this.token);
     let headers = {
       "content-type": "application/json",
-      Authorization: `Bearer ${this.token}` ?? undefined,
+      Authorization: `Bearer ${this.token}`,
       accept: "application/json",
     };
+    console.log("[DEBUG TBD] Headers so far...", headers);
 
     if (!!additionalHeaders) {
       headers = { ...headers, ...additionalHeaders };
     }
+    console.log("[DEBUG TBD] Final headers", headers);
     return headers;
   };
 
@@ -44,22 +59,17 @@ export class ApiRequest {
 
   createRequest = async <Type>(
     method: SupportedApiMethods,
-    endpoint: string,
+    endpoint: string, // that will be a full on env dependant url
     payload?: string,
     additionalHeaders?: { [key: string]: string }
   ): Promise<{ data: Type; statusCode: number }> => {
     const headers = this.requestHeaders(additionalHeaders);
 
-    const response = await fetch(
-      // `http://127.0.0.1:8000/api${endpoint}`,
-      // TODO: Move this to an env file
-      `https://api.staging.gamespace.gg/api${endpoint}`,
-      {
-        method: method,
-        body: payload,
-        headers,
-      }
-    );
+    const response = await fetch(endpoint, {
+      method: method,
+      body: payload,
+      headers,
+    });
 
     return await this.getResponse(response);
   };
