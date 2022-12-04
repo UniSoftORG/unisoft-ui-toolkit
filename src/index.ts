793f75b3a9d3a2f7459f343bf03a8f34a22dd636
PIYAtGitHub <petyr.yonkov@mail.bg>
2022-12-04 10:18:31 +0200

    [IMPORTANT] api is no longer exported directly!

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import elementWithPermissions, {
   pageWithPermissions,
 } from "./guards/permissionsGuard";
 import pageWithAuth, { elementWithAuth } from "./guards/authGuard";
-import { ApiRequest } from "./api/apiRequest";
 import { isGenericApiError } from "./guards/genericApiError";
 import { isLoginResponse } from "./guards/loginResponse";
 
@@ -11,6 +10,4 @@ const authGuard = { pageWithAuth, elementWithAuth };
 const permissionsGuard = { pageWithPermissions, elementWithPermissions };
 const typeGuard = { isGenericApiError, isLoginResponse };
 
-const api = new ApiRequest(""); // DO NOT USE THIS IN APPS. LOCAL UNAUTHENTICATED INSTANCE.
-
-export { sayHi, authGuard, permissionsGuard, typeGuard, api };
+export { sayHi, authGuard, permissionsGuard, typeGuard };
