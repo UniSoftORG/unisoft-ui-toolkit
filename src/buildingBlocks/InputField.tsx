631d18592c77a99645852880da02261bf04ab773
rade.iliev <rade.iliev@looming.tech>
2023-01-02 22:08:33 +0200

    Input & Quill changes

--- src/buildingBlocks/InputField.tsx
+++ src/buildingBlocks/InputField.tsx
@@ -3,37 +3,41 @@ import Error from "./Error";
 import { IInputFieldProps } from "../interfaces/generics";
 import React from "react";
 
-
 const InputField = (props: IInputFieldProps) => {
-  const {
-    fieldType,
-    inputValue,
-    onInputChange,
-    placeholder,
-    autoComplete,
-    error,
-    appearence,
-    errorCustomClassName,
-  } = props;
+    const {
+        fieldType,
+        inputValue,
+        onInputChange,
+        placeholder,
+        autoComplete,
+        error,
+        appearence,
+        errorCustomClassName,
+    } = props;
 
-  return (
-    <>
-      <input
-        className={`${appearence ?? ""}`}
-        placeholder={placeholder ?? "Input data here..."}
-        type={fieldType}
-        autoComplete={autoComplete ?? "off"}
-        value={inputValue}
-        onChange={(e) => {
-          const value = e.target.value;
-          onInputChange(value);
-        }}
-      />
-      {error ? (
-        <Error error={error} appearence={errorCustomClassName} />
-      ) : null}
-    </>
-  );
+    return (
+        <div className="w-full relative group">
+            <input type={fieldType}
+                   required
+                   value={inputValue}
+                   id={inputValue}
+                   autoComplete={autoComplete ?? "off"}
+                   className="w-full h-12 px-4 text-sm peer bg-dark-card outline-none"
+                   onChange={(e) => {
+                       const value = e.target.value;
+                       onInputChange(value);
+                   }}
+            />
+            <label htmlFor="eMail"
+                   className="transform transition-all absolute top-0 left-0 h-full flex items-center pl-2 text-sm group-focus-within:text-xs peer-valid:text-xs group-focus-within:h-1/2 peer-valid:h-1/2 group-focus-within:-translate-y-full peer-valid:-translate-y-full group-focus-within:pl-0 peer-valid:pl-0">{props.label}</label>
+
+            {/*{error ? (*/}
+            {/*    <Error error={error} appearence={errorCustomClassName} />*/}
+            {/*) : null}*/}
+
+        </div>
+    );
 };
 
 export default InputField;
+
--- src/buildingBlocks/QuillEditor.tsx
+++ src/buildingBlocks/QuillEditor.tsx
@@ -2,41 +2,41 @@ import dynamic from "next/dynamic";
 import React from "react";
 
 interface IQuillEditorProps {
-  appearence: string; 
+  appearence: string;
   onQuillChanged: (value: string) => void;
   placeholder?: string;
+  readOnly?: boolean;
 }
 const QuillEditor = (props: IQuillEditorProps) => {
   return (
-    <QuillNoSSRWrapper
-      className={props.appearence}
-      theme="snow"
-      modules={modules}
-      formats={formats}
-      onChange={(v) => {
-        props.onQuillChanged(v);
-      }}
-      placeholder={
-        props.placeholder ??
-        "A beautiful post is a simple, yet eloquent one. Let's see what you've got!"
-      }
-    />
+      <QuillNoSSRWrapper
+          className={props.appearence}
+          theme="snow"
+          readOnly={props.readOnly}
+          modules={modules}
+          formats={formats}
+          onChange={(v) => {
+            props.onQuillChanged(v);
+          }}
+          placeholder={
+              props.placeholder ??
+              "A beautiful post is a simple, yet eloquent one. Let's see what you've got!"
+          }
+      />
   );
 };
 
 const modules = {
   toolbar: [
-    [{ header: "1" }, { header: "2" }, { font: [] }],
-    [{ size: [] }],
-    ["bold", "italic", "underline", "strike", "blockquote"],
-    [
-      { list: "ordered" },
-      { list: "bullet" },
-      { indent: "-1" },
-      { indent: "+1" },
-    ],
-    ["link", "image"],
-    ["clean"],
+    [{ font: [] }, { size: [] }],
+    ['bold', 'italic', 'underline', 'strike' ],
+    [{ color: [] }, { background: [] }],
+    [{ script: 'super' }, { script: 'sub' }],
+    [{ header: '1' }, { header: '2' }, 'blockquote', 'code-block' ],
+    [{ list: 'ordered' }, { list: 'bullet'}, { 'indent': '-1' }, { 'indent': '+1' }],
+    [ 'direction', { align: [] }],
+    [ 'link', 'image', 'video', 'formula' ],
+    [ 'clean' ]
   ],
   clipboard: {
     // toggle to add extra line breaks when pasting HTML:
